d56cfda79d26676fea9c4449a482371322c03a9d
dmsqja <bum17822@naver.com>
2026-07-24 13:50:16 +0900

    fix: #51 app.js 머지 컨플릭트 마커 제거

--- src/app.js
+++ src/app.js
@@ -4,12 +4,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import swaggerUi from "swagger-ui-express";
 import authRouter from "#src/modules/auth/authRoute.js";
-<<<<<<< HEAD
 import likeRouter from "#src/modules/like/likeRoute.js";
-=======
 import translationRouter from "#src/modules/translation/translationRoute.js";
 import reviewRouter from "#src/modules/review/reviewRoute.js";
->>>>>>> bcf791ddabed21c9a84a8ad438b463f3a1df9598
 import errorHandler from "#src/common/middlewares/errorHandler.js";
 import { swaggerSpec } from "#src/common/configs/swagger.js";
 import { config } from "#src/common/configs/config.js";
@@ -24,12 +21,9 @@ app.use(cookieParser());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/auth", authRouter);
-<<<<<<< HEAD
 app.use("/like", likeRouter);
-=======
 app.use("/translation", translationRouter);
 app.use("/review", reviewRouter);
->>>>>>> bcf791ddabed21c9a84a8ad438b463f3a1df9598
 
 app.get("/", (req, res) => {
   res.send("서버가 잘 동작하고 있어요! 🎉");
